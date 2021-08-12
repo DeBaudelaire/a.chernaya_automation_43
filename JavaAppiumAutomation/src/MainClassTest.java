7b6c960980f07f95f3149bd8391c33ee598c0af4
DeBaudelaire <cute.little.lenore@gmail.com>
2021-08-12 23:32:33 +0300

    TEST 2
    Добавлен 2й тест к 1му уроку

--- JavaAppiumAutomation/src/MainClassTest.java
+++ JavaAppiumAutomation/src/MainClassTest.java
@@ -8,4 +8,15 @@ public class MainClassTest extends MainClass {
         int actual = getLocalNumber();
         Assert.assertTrue("getLocalNumber != 14", actual == expected);
     }
+
+    @Test
+    public void testGetClassNumber() {
+        int expectedmin = 45;
+        int actualnumber = getClassNumber();
+        if (actualnumber < expectedmin) {
+            Assert.fail("Oh, no! getClassNumber < 45");
+        }
+    }
 }
+
+
